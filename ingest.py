3af5c5425c2a5de5bcfbae069997899b2d401816
Alexandros Filothodoros <alexfilothodoros@gmail.com>
2023-08-21 13:04:45 +0200

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -1,29 +1,26 @@
-import glob
 import logging
 import os
-from concurrent.futures import ThreadPoolExecutor
-from multiprocessing import Pool
-from typing import List
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
 import click
 import torch
+from typing import List
 from langchain.docstore.document import Document
 from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import Language, RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
-from tqdm import tqdm
 
-from constants import (
+from constants2 import (
     CHROMA_SETTINGS,
     DOCUMENT_MAP,
     EMBEDDING_MODEL_NAME,
+    INGEST_THREADS,
     PERSIST_DIRECTORY,
     SOURCE_DIRECTORY,
 )
 
-print("This is an adaptation of imartinez/privateGPT#560 to localGPT")
-
-chunk_size = 100
-chunk_overlap = 10
+CHUNK_SIZE = 100
+CHUNK_OVERLAP = 10
 
 
 def load_single_document(file_path: str) -> List[Document]:
@@ -31,21 +28,29 @@ def load_single_document(file_path: str) -> List[Document]:
     if ext in DOCUMENT_MAP:
         loader_class, loader_args = DOCUMENT_MAP[ext]
         loader = loader_class(file_path, **loader_args)
-        
         return loader.load()
+    
 
 
 def load_document_batch(filepaths):
     logging.info("Loading document batch")
+    # create a thread pool
     with ThreadPoolExecutor(len(filepaths)) as exe:
+        # load files
         futures = [exe.submit(load_single_document, name) for name in filepaths]
+        # collect data
         data_list = [future.result() for future in futures]
-
+        # return data and file paths
         return (data_list, filepaths)
 
+from multiprocessing import Pool
+import glob
+from tqdm import tqdm
 
 def load_documents(source_dir: str, ignored_files: List[str] = []) -> List[Document]:
-
+    """
+    Loads all documents from the source documents directory, ignoring specified files
+    """
     all_files = []
     for ext in DOCUMENT_MAP:
         all_files.extend(
@@ -56,83 +61,62 @@ def load_documents(source_dir: str, ignored_files: List[str] = []) -> List[Docum
     with Pool(processes=os.cpu_count()) as pool:
         results = []
         with tqdm(total=len(filtered_files), desc='Loading new documents', ncols=80) as pbar:
-            for i, docs in enumerate(pool.imap_unordered(load_single_document, filtered_files)):
+            for docs in pool.imap_unordered(load_single_document, filtered_files):
                 results.extend(docs)
                 pbar.update()
 
     return results
 
+def process_documents(ignored_files: List[str] = []) -> List[Document]:
+    """
+    Load documents and split in chunks
+    """
+    print(f"Loading documents from {SOURCE_DIRECTORY}")
+    documents = load_documents(SOURCE_DIRECTORY, ignored_files)
+    if not documents:
+        print("No new documents to load")
+        exit(0)
+    print(f"Loaded {len(documents)} new documents from {SOURCE_DIRECTORY}")
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
+    texts = text_splitter.split_documents(documents)
+    print(f"Split into {len(texts)} chunks of text (max. {CHUNK_SIZE} tokens each)")
+    return texts
 
-def split_documents(documents: list[Document]) -> tuple[list[Document], list[Document]]:
-    # Splits documents for correct Text Splitter
-    text_docs, python_docs = [], []
-    for doc in documents:
-        file_extension = os.path.splitext(doc.metadata["source"])[1]
-        if file_extension == ".py":
-            python_docs.append(doc)
-        else:
-            text_docs.append(doc)
+def does_vectorstore_exist(persist_directory: str) -> bool:
+    """
+    Checks if vectorstore exists
+    """
+    if os.path.exists(os.path.join(persist_directory, 'index')):
+        if os.path.exists(os.path.join(persist_directory, 'chroma-collections.parquet')) and os.path.exists(os.path.join(persist_directory, 'chroma-embeddings.parquet')):
+            list_index_files = glob.glob(os.path.join(persist_directory, 'index/*.bin'))
+            list_index_files += glob.glob(os.path.join(persist_directory, 'index/*.pkl'))
+            # At least 3 documents are needed in a working vectorstore
+            if len(list_index_files) > 3:
+                return True
+    return False
 
-    return text_docs, python_docs
+def main():
+    # Create embeddings
+    embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
 
-
-@click.command()
-@click.option(
-    "--device_type",
-    default="cuda" if torch.cuda.is_available() else "cpu",
-    type=click.Choice(
-        [
-            "cpu",
-            "cuda",
-            "ipu",
-            "xpu",
-            "mkldnn",
-            "opengl",
-            "opencl",
-            "ideep",
-            "hip",
-            "ve",
-            "fpga",
-            "ort",
-            "xla",
-            "lazy",
-            "vulkan",
-            "mps",
-            "meta",
-            "hpu",
-            "mtia",
-        ],
-    ),
-    help="Device to run on. (Default is cuda)",
-)
-
-
-def main(device_type):
-    logging.info(f"Loading documents from {SOURCE_DIRECTORY}")
-    documents = load_documents(SOURCE_DIRECTORY)
-    text_documents, python_documents = split_documents(documents)
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    python_splitter = RecursiveCharacterTextSplitter.from_language(
-        language=Language.PYTHON, chunk_size=880, chunk_overlap=200
-    )
-    texts = text_splitter.split_documents(text_documents)
-    texts.extend(python_splitter.split_documents(python_documents))
-    logging.info(f"Loaded {len(documents)} documents from {SOURCE_DIRECTORY}")
-    logging.info(f"Split into {len(texts)} chunks of text")
-
-    embeddings = HuggingFaceInstructEmbeddings(
-        model_name=EMBEDDING_MODEL_NAME,
-        model_kwargs={"device": device_type},
-    )
-
-    db = Chroma.from_documents(
-        texts,
-        embeddings,
-        persist_directory=PERSIST_DIRECTORY,
-        client_settings=CHROMA_SETTINGS,
-    )
+    if does_vectorstore_exist(PERSIST_DIRECTORY):
+        # Update and store locally vectorstore
+        print(f"Appending to existing vectorstore at {PERSIST_DIRECTORY}")
+        db = Chroma(persist_directory=PERSIST_DIRECTORY, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
+        collection = db.get()
+        texts = process_documents([metadata['source'] for metadata in collection['metadatas']])
+        print(f"Creating embeddings. May take some minutes...")
+        db.add_documents(texts)
+    else:
+        # Create and store locally vectorstore
+        print("Creating new vectorstore")
+        texts = process_documents()
+        print(f"Creating embeddings. May take some minutes...")
+        db = Chroma.from_documents(texts, embeddings, persist_directory=PERSIST_DIRECTORY, client_settings=CHROMA_SETTINGS)
     db.persist()
     db = None
+
+    print(f"Ingestion complete!")
 
 
 if __name__ == "__main__":
